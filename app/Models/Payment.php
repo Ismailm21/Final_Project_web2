df8d437549fee09df9ffe9e43d9071653e577278
Ismail <202210756@ua.edu.lb>
2025-05-04 13:30:16 +0300

    migrations done

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -1,15 +1,22 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    public function getClient(){
-        return $this->belongsTo(Client::class,'client_id','id');
+    protected $fillable = [
+        'client_id', 'amount', 'currency',
+        'payment_method', 'transaction_id', 'status'
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
     }
-    public function getOrder(){
-        return $this->belongsTo(Order::class);
+
+    public function order()
+    {
+        return $this->hasOne(Order::class);
     }
 }
