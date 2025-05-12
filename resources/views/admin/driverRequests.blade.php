94b3c55a70e9697b230ef7278a2385623aa2a284
Ismail <202210756@ua.edu.lb>
2025-05-12 12:02:17 +0300

    message

--- resources/views/admin/driverRequests.blade.php
+++ resources/views/admin/driverRequests.blade.php
@@ -20,23 +20,32 @@
                 <th>Email</th>
                 <th>Phone</th>
                 <th>Action</th>
+                <th>Day</th>
+                <th>Start Time</th>
+                <th>End Time</th>
             </tr>
             </thead>
             <tbody>
             @foreach ($pending_drivers as $driver)
                 <tr class="border-b">
                     <td class="p-2">{{ $driver->name }}</td>
-                    <td>{{ $driver->email }}</td>
-                    <td>{{ $driver->phone }}</td>
-                    <td>{{ $driver->license_number }}</td>
+                    <td class="p-2">{{ $driver->email }}</td>
+                    <td class="p-2">{{ $driver->phone }}</td>
+                    <td class="p-2">{{ $driver->license_number }}</td>
+                    @foreach($pending_availabilities as $pending_availability )
+                        @php$driver_availabilities = $pending_availabilities->where('pending_id', $driver->id); @endphp
+                        <td class="p-2">{{ $availability->day }}</td>
+                        <td class="p-2">{{ $availability->start_time }}</td>
+                        <td class="p-2">{{ $availability->end_time }}</td>
+                    @endforeach
                     <td class="flex gap-2">
                         <form action="{{ route('admin.driver.request.handle', ['id' => $driver->id, 'action' => 'approve']) }}" method="POST">
                             @csrf
-                            <button class="bg-green-500 text-white px-3 py-1 rounded">Approve</button>
+                            <button id="approve" class="bg-green-500 text-white px-3 py-1 rounded">Approve</button>
                         </form>
                         <form action="{{ route('admin.driver.request.handle', ['id' => $driver->id, 'action' => 'deny']) }}" method="POST">
                             @csrf
-                            <button class="bg-red-500 text-white px-3 py-1 rounded">Deny</button>
+                            <button id="deny" class="bg-red-500 text-white px-3 py-1 rounded">Deny</button>
                         </form>
                     </td>
                 </tr>
