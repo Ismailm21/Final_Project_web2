ccbac5a1c06eae0d8fd0bb64965a4675418d3170
Ismail <202210756@ua.edu.lb>
2025-05-12 16:52:36 +0300

    migrations are all done

--- database/migrations/2025_04_26_193337_create_areas_table.php
+++ database/migrations/2025_04_26_193337_create_areas_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('areas', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 10, 8);
             $table->timestamps();
         });
     }
