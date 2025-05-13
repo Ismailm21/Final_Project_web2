4072ba7cb724e5ffc3d8f40979e0a228223f58ee
Raed <chehaitli.raed@gmail.com>
2025-05-13 14:45:12 +0300

    edded signin with github

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -25,6 +25,11 @@ class SocialiteController extends Controller
         return Socialite::driver('facebook')->redirect();
     }
 
+    public function redirectToProvider()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
     public function handleGoogleCallback()
     {
         try {
@@ -80,6 +85,35 @@ class SocialiteController extends Controller
             }
         } catch (\Exception $e) {
             return redirect()->route('client.login')->withErrors(['msg' => 'Facebook authentication failed.']);
+        }
+    }
+
+    public function handleGitHubCallback()
+    {
+        try {
+            $user = Socialite::driver('github')->stateless()->user();
+
+            $finduser = User::where('social_id', $user->getId())->first();
+
+            if ($finduser) {
+                Auth::login($finduser);
+                return "Welcome back, client ID: " . $finduser->id;
+            } else {
+                $newuser = User::create([
+                    'name' => $user->name ?? $user->nickname, // Fallback to nickname if no name
+                    'email' => $user->email,
+                    'role' => 'client',
+                    'social_id' => $user->getId(),
+                    'social_type' => 'github',
+                    'is_verified' => true,
+                    'password' => Hash::make('my-github'), // Dummy password
+                ]);
+
+                Auth::login($newuser);
+                return "Welcome, new client ID: " . $newuser->id;
+            }
+        } catch (\Exception $e) {
+            return redirect()->route('client.login')->withErrors(['msg' => 'GitHub authentication failed.']);
         }
     }
 
